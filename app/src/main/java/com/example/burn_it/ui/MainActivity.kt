2e8cb5ff5fa7e490d6793e0a54f43a45522d66e4
osualaFC <fredrick.osuala@decagon.dev>
2020-11-18 13:48:14 +0100

    designed layout

--- app/src/main/java/com/example/burn_it/ui/MainActivity.kt
+++ app/src/main/java/com/example/burn_it/ui/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.burn_it.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.burn_it.R
 import com.example.burn_it.databinding.ActivityMainBinding
 
@@ -13,5 +16,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        val controller = binding.navHostFragment.findNavController()
+        val bottomNav =  binding.bottomNavigationView
+
+       bottomNav.setupWithNavController(controller)
+
+       controller.addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNav.visibility = View.VISIBLE
+                    else -> bottomNav.visibility = View.GONE
+                }
+            }
     }
 }
