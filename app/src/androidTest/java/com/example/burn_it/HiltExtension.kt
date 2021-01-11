9ddf22793bccb226d0f5ab88570e90f92fa8d830
fredrick osuala <fredosuala@gmail.com>
2021-01-11 11:55:25 +0100

    .

--- app/src/androidTest/java/com/example/burn_it/HiltExtension.kt
+++ app/src/androidTest/java/com/example/burn_it/HiltExtension.kt
@@ -40,36 +40,7 @@ inline fun <reified T : Fragment> launchFragmentInHiltContainer(
     }
 }
 
-//@ExperimentalCoroutinesApi
-//inline fun <reified T : Fragment> launchFragmentInHiltContainer(
-//    fragmentArgs: Bundle? = null,
-//    @StyleRes themeResId: Int = R.style.FragmentScenarioEmptyFragmentActivityTheme,
-//    factory: FragmentFactory,
-//    crossinline action: Fragment.() -> Unit = {}
-//) {
-//    val startActivityIntent = Intent.makeMainActivity(
-//        ComponentName(
-//            ApplicationProvider.getApplicationContext(),
-//            HiltTestActivity::class.java
-//        )
-//    ).putExtra(FragmentScenario.EmptyFragmentActivity.THEME_EXTRAS_BUNDLE_KEY, themeResId)
-//
-//    ActivityScenario.launch<HiltTestActivity>(startActivityIntent).onActivity { activity ->
-//        activity.supportFragmentManager.fragmentFactory = factory
-//        val fragment: Fragment = activity.supportFragmentManager.fragmentFactory.instantiate(
-//            Preconditions.checkNotNull(T::class.java.classLoader),
-//            T::class.java.name
-//        )
-//        fragment.arguments = fragmentArgs
-//
-//        activity.supportFragmentManager
-//            .beginTransaction()
-//            .add(android.R.id.content, fragment, "")
-//            .commitNow()
-//
-//        fragment.action()
-//    }
-//}
+
 
 //@ExperimentalCoroutinesApi
 //inline fun <reified T : Fragment> launchFragmentInHiltContainer(
